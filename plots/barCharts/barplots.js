f27750e0463ea241d625a03a4edee6eb87e8c646
sag7221 <toshilpa@gmail.com>
2022-12-04 23:19:15 -0800

    bar charts with OTUs ids and values

--- plots/barCharts/barplots.js
+++ plots/barCharts/barplots.js
@@ -4,6 +4,7 @@ function init() {
 
   // Use the list of sample names to populate the select options
   d3.json("samples.json").then((data) => {
+    console.log("data: ", data)
     var sampleNames = data.names;
 
     sampleNames.forEach((sample) => {
@@ -61,24 +62,41 @@ function buildCharts(sample) {
     var samples = data.samples;
     // 4. Create a variable that filters the samples for the object with the desired sample number.
     var sampleArray = samples.filter(sampleObj => sampleObj.id == sample);
+    console.log("sampleArray: ", sampleArray)
     //  5. Create a variable that holds the first sample in the array.
     var sampleFirst = sampleArray[0]
-
+    console.log("sampleFirst: ", sampleFirst)
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
     let otu_ids = sampleFirst.otu_ids;
-    let otu_labels = sampleFirst.otu_labels;
-    let sample_values = sampleFirst.sample_values;
+    console.log("otu_ids: ", otu_ids)
+    let otu_labels = sampleFirst.otu_labels.slice(0,10).reverse();
+    console.log("otu_labels: ",otu_labels)
+    let sample_values = sampleFirst.sample_values.slice(0,10).reverse();
+    console.log("sample_values: ",sample_values)
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
+    top_otu_ids = otu_ids.slice(0,10)
+    console.log("top_otu_ids: ", top_otu_ids)
+    top_otu_ids = top_otu_ids.map(otu_id => "OTU "+otu_id);
+    console.log("top_otu_ids: ", top_otu_ids)
+    top_otu_labels = otu_labels.slice(0,10)
+    // sample_values_sorted = data.samples.map(sample => sample.sample_values).sort((a,b) => b-a);
+    // filteredWreq = otu_details.filter(element => element != null);
+    // console.log(filteredWreq);
+    // console.log("sample_values_sorted: ",sample_values_sorted.slice(0,10))
 
-    var yticks = otu_labels;
+    // var yticks = top_otu_labels;
+    var yticks = top_otu_ids
+    console.log("yticks: ", yticks)
 
     let trace = {
       x: sample_values,
-      y: otu_ids,
-      type: "bar"
+      y: yticks,
+      orientation: "h",
+      type: "bar",
+      text: otu_labels
     }
 
     // 8. Create the trace for the bar chart. 
@@ -86,10 +104,11 @@ function buildCharts(sample) {
     // 9. Create the layout for the bar chart. 
     var barLayout = {
       title: "Top 10 Bacterial Species",
-      xaxis: {title: "sample values"},
-      yaxis: {ticks: yticks, showticklabels: true}
+      // xaxis: {ticks: yticks, showticklabels: true},
+      // yaxis: {title: "sample values", ticks: otu_ids, showticklabels: true}
     };
     // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot("plotArea", barData, barLayout);
   });
+
 }
